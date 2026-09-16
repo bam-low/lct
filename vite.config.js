665804db7910d6eac8368aab2ac5cd5b65c0a37f
Egor <lysyhe.vrn@gmail.com>
2026-09-16 23:33:21 +0300

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  base: '/lct/',
+  plugins: [react(), tailwindcss()],
+})
